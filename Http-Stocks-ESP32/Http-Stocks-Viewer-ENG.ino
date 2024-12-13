ae503939510c561014ef4746f0627a25afa641f1
impap-byte <gokturkbnayytb@gmail.com>
2024-12-14 00:56:55 +0500

    Improved Code Clarity

--- Http-Stocks-ESP32/Http-Stocks-Viewer-ENG.ino
+++ Http-Stocks-ESP32/Http-Stocks-Viewer-ENG.ino
@@ -5,14 +5,26 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#define BUTTON_BITMASK 0b10000000010000
-#define RST_PIN 15
-#define SS_PIN 5
+
+// Definitions for the RFID reader
+#define BUTTON_BITMASK 0b10000000010000 // Button bitmask for wake-up
+#define RST_PIN 15 // Reset pin for MFRC522
+#define SS_PIN 5   // Slave Select pin for MFRC522
+
+// Create MFRC522 instance
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// Variable to check if it's the first run
 RTC_DATA_ATTR bool first_run = true;
+
+// Initialize stock index
 int stocks = 0;
+
+// WiFi credentials
 const char* ssid = "ssid";
 const char* password = "password";
+
+// API URLs for stock data and percentages
 const char* apiURLdardanel = "";
 const char* apiURLdnisi = "";
 const char* apiURLeczaci = "";
@@ -22,48 +34,67 @@ const char* apiURLdnisi_percentage = "";
 const char* apiURLeczaci_percentage = "";
 const char* apiURLsoke_percentage = "";
 
+// Initialize LCD with I2C address and dimensions
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Arrays to hold API URLs
 const char* stockURLs[4];
 const char* percentages[4];
+
+// Timers for updating stocks and handling button presses
 unsigned long previousTime = 0;
 const long waitTime = 30000;
 
+// Button and switch pins
 const int buttonForward = 13;
 const int buttonBack = 4;
 const int sleepSwitch = 16;
 
+// Variables for button states
 int counter = 0;
 unsigned long buttonTime = 0;
 volatile bool forwardPressed = false;
 volatile bool backPressed = false;
+
+// Time before entering sleep mode after no button presses
 unsigned long buttonWaitTime = 60000;
+
 // Function prototypes
 void showStock(int stock);
 void backInterrupt();
 void forwardInterrupt();
 
 void setup() {
+  // Initialize LCD
   lcd.init();
   lcd.backlight();
+
+  // First run: RFID card authentication
   if (first_run) {
     SPI.begin();
     mfrc522.PCD_Init();
     lcd.print("   STOCKMATIC");
     lcd.setCursor(0, 1);
     lcd.print("WAITING FOR CARD");
+
     while (first_run) {
+      // Wait for a new card to be presented
       if (!mfrc522.PICC_IsNewCardPresent()) {
         continue;
       }
       if (!mfrc522.PICC_ReadCardSerial()) {
         continue;
       }
+
+      // Read RFID card content
       String content = "";
       for (byte i = 0; i < mfrc522.uid.size; i++) {
         content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " "));
         content.concat(String(mfrc522.uid.uidByte[i], HEX));
       }
       content.toUpperCase();
+
+      // Check if the card is authorized
       lcd.clear();
       if (content.substring(1) == "82 B5 5D 51") {
         lcd.print("    HELLO");
@@ -81,37 +112,45 @@ void setup() {
         continue;
       }
     }
+    // Shutdown RFID reader after authentication
     mfrc522.PICC_HaltA();
     mfrc522.PCD_AntennaOff();
     mfrc522.PCD_SoftPowerDown();
     SPI.end();
   }
+
+  // Enable wakeup using button bitmask
   esp_sleep_enable_ext1_wakeup(BUTTON_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
   delay(1000);
 
+  // Assign API URLs to arrays
   stockURLs[0] = apiURLdardanel;
   stockURLs[1] = apiURLdnisi;
   stockURLs[2] = apiURLeczaci;
   stockURLs[3] = apiURLsoke;
-
   percentages[0] = apiURLdardanel_percentage;
   percentages[1] = apiURLdnisi_percentage;
   percentages[2] = apiURLeczaci_percentage;
   percentages[3] = apiURLsoke_percentage;
+
+  // Set button and switch pins
   pinMode(buttonBack, INPUT);
   pinMode(buttonForward, INPUT);
   pinMode(sleepSwitch, INPUT_PULLUP);
+
+  // Attach interrupts for button presses
   attachInterrupt(buttonBack, backInterrupt, RISING);
   attachInterrupt(buttonForward, forwardInterrupt, RISING);
 
+  // Connect to WiFi
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     lcd.setCursor(0, 0);
     lcd.print("  CONNECTING...");
   }
 
+  // Indicate WiFi connection
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("WiFi CONNECTED!");
@@ -122,6 +161,7 @@ void setup() {
 }
 
 void loop() {
+  // Handle forward button press
   if (forwardPressed) {
     buttonTime = millis();
     forwardPressed = false;
@@ -135,6 +175,7 @@ void loop() {
     }
   }
 
+  // Handle back button press
   if (backPressed) {
     buttonTime = millis();
     lcd.backlight();
@@ -147,6 +188,8 @@ void loop() {
       showStock(stocks);
     }
   }
+
+  // Update stock data periodically
   unsigned long currentTime = millis();
   if (currentTime - previousTime >= waitTime) {
     counter++;
@@ -168,16 +211,14 @@ void loop() {
       if (httpResponseCode > 0) {
         String response2 = http2.getString();
         response2.trim();
-        if (response2.indexOf("Error parsing document, try a different parse string.") > -1)
-        {
+        if (response2.indexOf("Error parsing document, try a different parse string.") > -1) {
           lcd.print("     ");
           lcd.print("%0,00");
           http2.end();
-        }
-        else{
-        lcd.print("     ");
-        lcd.print(response2);
-        http2.end();
+        } else {
+          lcd.print("     ");
+          lcd.print(response2);
+          http2.end();
         }
       } else {
         lcd.clear();
@@ -194,6 +235,7 @@ void loop() {
   }
 }
 
+// Function to display stock data
 void showStock(int stock) {
   HTTPClient http;
   http.begin(stockURLs[stock]);
@@ -227,10 +269,12 @@ void showStock(int stock) {
   }
 }
 
+// Interrupt service routine for back button
 void IRAM_ATTR backInterrupt() {
   backPressed = true;
 }
 
+// Interrupt service routine for forward button
 void IRAM_ATTR forwardInterrupt() {
   forwardPressed = true;
 }
